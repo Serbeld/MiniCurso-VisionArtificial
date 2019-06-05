e515d0f1b52cbb06a426dcf4911ea3e0922ca057
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-06-04 23:35:59 -0500

    Update Tomar_Video.py

--- Tomar_Video.py
+++ Tomar_Video.py
@@ -1,29 +1,22 @@
 # encoding: utf-8 
 
 # Tomar_Video.py
-
-# Este código va orientado a la toma de imágenes de video mediante 
-# la librería open cv en Python...
+# Este código va orientado a la toma de imágenes de video mediante la librería open cv en Python...
 
 # Programador Sergio Luis Beleño Díaz
 # 05.Junio.2019
 
-'''
-Para empezar se importan las librerías de Open cv para visión 
-Artificial y se utiliza la librería numpy para la optimización 
-de datos al trabajar con las matrices que componen a la imagen 
-obtenida pixel por pixel
-'''
+#Para empezar se importa la librería de Open cv para visión Artificial
 
 import cv2
 
-# Asignamos la camara ingresando cv2.VideoCapture(0)
-# Si quiere asignar una segunda camara externa puede usar cv2.VideoCapture(1)
+# Asignamos la cámara ingresando cv2.VideoCapture(0)
+# Si quiere asignar una segunda cámara externa puede usar cv2.VideoCapture(1)
 cap = cv2.VideoCapture(0)
 
 while(True):
 
-	# Toma parametros de captura de la camara
+	# Toma parámetros de captura de la cámara
 	[rec, camara] = cap.read()
 
 	# Muestra la imagen tomada en una ventana
