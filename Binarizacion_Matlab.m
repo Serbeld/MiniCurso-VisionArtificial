23019a367d33062eac7cc3ccba27db526d40b589
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-06-26 00:09:30 -0500

    Delete Binarizacion_Matlab.m

--- Binarizacion_Matlab.m
+++
@@ -1,9 +0,0 @@
-clc; clear all;              % Limpialas variables almacenadas y el command windows
-I = imread('camino.jpg');    % Carga la imagen
-ibn=im2bw(I,0.44);           % Binarización
-figure (1)
-imshow(I)                    % Muestra la imagen RGB
-figure(2)
-imshow(ibn)                  % Muestra la imagen binarizada
-figure(3)
-imtool(ibn)                  % Muestra las características de la imagen binarizada
