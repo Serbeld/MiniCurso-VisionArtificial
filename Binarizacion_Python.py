a7285a16089bc60d5c51083dfd736b056d688a61
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-07-19 08:02:09 -0500

    Binarizacion_Python

--- Binarizacion_Python.py
+++ Binarizacion_Python.py
@@ -1,14 +1,15 @@
 import cv2
 
 imagen = cv2.imread('opencv_logo.png')
-Rango_bajo = 127
-Rango_alto = 255
+Rango_de_binarizacion = 40
+PixelON = 255
 
 gray = cv2.imread('opencv_logo.png', cv2.IMREAD_GRAYSCALE)
-ret,th1 = cv2.threshold(gray,Rango_bajo,Rango_alto,cv2.THRESH_BINARY)
+ret,binarizada = cv2.threshold(gray,Rango_de_binarizacion,PixelON,cv2.THRESH_BINARY)
+print((ret))
 
 cv2.imshow('Original',imagen)
 cv2.imshow('Escala de grises',gray)
-cv2.imshow('Binarizada',th1)
+cv2.imshow('Binarizada',binarizada)
 
 cv2.waitKey(0)
